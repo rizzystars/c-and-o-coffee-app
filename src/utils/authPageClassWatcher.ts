eeed00b017a32e2a3f5cacba1855035f292155f9
Michael Torres <mixedrootsmaryland@gmail.com>
2025-10-11 20:08:21 -0400

    feat(auth-mobile v2b): CSS + watcher; entry import (root entry)

---
+++ src/utils/authPageClassWatcher.ts
@@ -0,0 +1,17 @@
+﻿export function initAuthPageClassWatcher() {
+  function update() {
+    const loc = (location.pathname || "") + (location.hash || "");
+    const isAuth = /(^|\/)(login|signup|forgot)\b/i.test(loc);
+    document.body.classList.toggle("auth-page", !!isAuth);
+  }
+  window.addEventListener("hashchange", update);
+  window.addEventListener("popstate", update);
+  const _pushState = history.pushState;
+  history.pushState = function() {
+    // @ts-ignore
+    const r = _pushState.apply(this, arguments as any);
+    update();
+    return r;
+  };
+  update();
+}
